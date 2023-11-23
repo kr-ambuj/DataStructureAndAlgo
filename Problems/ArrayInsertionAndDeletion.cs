b75a803cfd05b7e06d3b72ed83a8b9f7a6cc0811
kr-ambuj <kumar.ambuj@hotmail.com>
2023-11-23 14:49:32 +0530

    Add project files.

---
+++ Problems/ArrayInsertionAndDeletion.cs
@@ -0,0 +1,54 @@
+﻿namespace DataStructureAndAlgo.Problems
+{
+    internal class ArrayInsertionAndDeletion
+    {
+        int[] intArray;
+        int len;
+        public ArrayInsertionAndDeletion(int length)
+        {
+            intArray = new int[length];
+            for (int i = 0; i < length; i++)
+                intArray[i] = i;
+            len = length;
+            PrintArray();
+        }
+
+        internal void Insert(int val, int pos)
+        {
+            Console.WriteLine();
+            if (pos == len - 1)
+                intArray[pos] = val;
+            else if (pos > len - 1)
+                Console.WriteLine("Postion to replace the value is larger than the length of Array, no change to array!!");
+            else
+            {
+                for (int i = len - 2; i > pos - 1; i--)
+                    intArray[i + 1] = intArray[i];
+                intArray[pos] = val;
+            }
+
+            PrintArray();
+        }
+
+        internal void Delete(int pos)
+        {
+            Console.WriteLine();
+            if (pos == len - 1)
+                intArray[pos] = 0;
+            else if (pos > len - 1)
+                Console.WriteLine("Position to delete is larger than the length of array, no chnage to array!!");
+            else
+            {
+                for (int i = pos; i < len - 1; i++)
+                    intArray[i] = intArray[i + 1];
+                intArray[len - 1] = 0;
+            }
+            PrintArray();
+        }
+
+        private void PrintArray()
+        {
+            Console.WriteLine(string.Join(',', intArray));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using DataStructureAndAlgo.Problems;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Data Structure and Algorithms!");
+
+# region Array Manipulation
+ArrayInsertionAndDeletion arrayInsAndDel = new ArrayInsertionAndDeletion(10);
+arrayInsAndDel.Insert(25, 6);
+arrayInsAndDel.Insert(99, 9);
+arrayInsAndDel.Insert(13, 10);
+arrayInsAndDel.Insert(17, 11);
+arrayInsAndDel.Delete(7);
+arrayInsAndDel.Delete(0);
+arrayInsAndDel.Delete(1);
+arrayInsAndDel.Delete(12);
+arrayInsAndDel.Insert(99, 9);
+arrayInsAndDel.Delete(9);
+#endregion
